b2a995e204856ab8821f1ac3b158fdd594cefaa6
moonishteapot <cole.d.vick@gmail.com>
2019-02-20 18:59:00 -0500

    those bastards won't let me see the doctoraladvisors

--- scrape.py
+++ scrape.py
@@ -6,12 +6,21 @@ div_only.seek(0)
 div_only.truncate()
 
 soup = BeautifulSoup(george.read(), 'html.parser')
-
 sidebar_details_tag = soup.findAll("table", {"class": "infobox biography vcard"})
 
+sidebar_details_table = soup.findChildren('table')[0]
+rows = sidebar_details_table.findChildren('tr')
+
+for row in rows: 
+    if row.findChildren('th'):  
+        for linebreak in row.find_all('br'):
+            linebreak.extract()
+        for th in row.findChildren('th'):
+            print(th.string)
+            print('---------------')
+
+
 div_only.write(str(sidebar_details_tag))
-
-
 
 div_only.close()
 
