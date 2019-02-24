e7e957272852421e4892dbafa91d92fe32e3d4b0
moonishteapot <cole.d.vick@gmail.com>
2019-02-23 21:34:27 -0500

    tentative progress in d3

--- web/scholars.js
+++ web/scholars.js
@@ -1,3 +1,6 @@
+// set data file
+scholar_data = "./george_scholars.json"
+
 // set up SVG for D3
 const width = 960;
 const height = 500;
@@ -9,12 +12,14 @@ const svg = d3.select('body')
   .attr('height', height);
 
 let scholars;
-const links = new Set([]);
+const links = new Set();
 
-async function dataLoadAndSetup() {
-  d3.json("./scholars.json", function(data) {
+function dataLoadAndSetup() {
+  d3.json(scholar_data, function(data) {
     scholars = data;
+    console.log(scholars);
     setUpLinks();
+    console.log(links);
   });
 }
 
@@ -22,21 +27,20 @@ function findScholar(scholar) {
   let found_scholar = scholars.filter((doc) => {
     return doc.name === scholar["name"];
   });
-  links.push({source: found_scholar, target: scholar});
+  links.add({source: found_scholar, target: scholar});
 }
 
 function setUpLinks() {
-
-  console.log('adsf');
-  for (const scholar in scholars) {
-    console.log('adsf');
-    for (const doc_advisor in scholar["doctoral_advisors"]) {
+  for (let i = 0; i < scholars.length; i++) {
+    cur_scholar = scholars[i];
+    for (let doc_advisor in cur_scholar["doctoral_advisors"]) {
       findScholar(doc_advisor);
     }
-    for (const academic_advisor in scholar["academic_advisors"]) {
+    for (let academic_advisor in cur_scholar["academic_advisors"]) {
       findScholar(academic_advisor);
     }
   }
 }
 
 dataLoadAndSetup();
+console.log()
