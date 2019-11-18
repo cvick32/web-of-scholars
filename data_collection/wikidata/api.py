182764a17d17cf3a8ea8f6e26df55a09cb93ed02
cole <cole.d.vick@gmail.com>
2019-11-18 13:57:24 -0500

    moved functionality to separate functions, i still think this could be made better though, some sort of abstraction over what you're accessing with the scholar

--- data_collection/wikidata/api.py
+++ data_collection/wikidata/api.py
@@ -39,78 +39,89 @@ class WikiDataScholars:
     def get_scholar_advisors_and_students(self, scholar_query_id):
         cur_scholar = self.Wiki_Client.get(scholar_query_id)
         print(scholar_query_id)
-        # try to get scholar's name
-        try:
-            cur_scholar_name = cur_scholar.attributes["aliases"]["en"][0]["value"]
-            print(cur_scholar_name)
-        except (TypeError, KeyError):
-            try:
-                cur_scholar_name = cur_scholar.attributes["labels"]["en"]["value"]
-                print(cur_scholar_name)
-            except (TypeError, KeyError):
-                self.debug.write("NO NAME: " + scholar_query_id + "\n")
-                # if this scholar does not have a name, do nothing else
-                return
-        # try to get scholar's image
-        try:
-            cur_scholar_image = WIKIDATA_IMAGE + cur_scholar.attributes["claims"][
-                property_map["image"]
-            ][0]["mainsnak"]["datavalue"]["value"].replace(" ", "_")
-        except (TypeError, KeyError):
-            self.debug.write("NO IMAGE: " + scholar_query_id + "\n")
-            cur_scholar_image = ""
-        # try to get scholar's link to wiki, only accept english wiki currently
-        try:
-            cur_scholar_link = cur_scholar.attributes["sitelinks"]["enwiki"]["url"]
-        except (TypeError, KeyError):
-            self.debug.write("NO WIKI LINK: " + scholar_query_id + "\n")
-            cur_scholar_link = ""
 
-        # try to get scholar's academic field
-        try:
-            cur_scholar_field = cur_scholar.attributes["claims"][property_map["field"]][
-                0
-            ]["mainsnak"]["datavalue"]["value"]["id"]
-        except (TypeError, KeyError):
-            self.debug.write("NO FIELD: " + scholar_query_id + "\n")
-            cur_scholar_field = ""
+        scholar_name = self.get_name(cur_scholar, scholar_query_id)
+        scholar_wiki_link = self.get_wiki_link(cur_scholar, scholar_query_id)
+        scholar_image = self.get_image(cur_scholar, scholar_query_id)
+        scholar_field = self.get_field(cur_scholar, scholar_query_id)
+        scholar_advisors = self.get_advisors(cur_scholar, scholar_query_id)
+        scholar_students = self.get_students(cur_scholar, scholar_query_id)
 
         cur_scholar_json = {
             "id": scholar_query_id,
-            "name": cur_scholar_name,
-            "image": cur_scholar_image,
-            "link": cur_scholar_link,
-            "field": cur_scholar_field,
-            "doctoral_advisor": list(),
-            "doctoral_student": list(),
+            "name": scholar_name,
+            "image": scholar_image,
+            "link": scholar_wiki_link,
+            "field": scholar_field,
+            "doctoral_advisor": scholar_advisors,
+            "doctoral_student": scholar_students,
         }
-
-        # try to get scholar advisors
-        try:
-            for advisor in cur_scholar.attributes["claims"][
-                property_map["doctoral_advisor"]
-            ]:
-                advisor_qid = advisor["mainsnak"]["datavalue"]["value"]["id"]
-                self.to_process.append(advisor_qid)
-                cur_scholar_json["doctoral_advisor"].append(advisor_qid)
-        except KeyError:
-            pass
-
-        # try to get scholar students
-        try:
-            for student in cur_scholar.attributes["claims"][
-                property_map["doctoral_student"]
-            ]:
-                student_qid = student["mainsnak"]["datavalue"]["value"]["id"]
-                self.to_process.append(student_qid)
-                cur_scholar_json["doctoral_student"].append(student_qid)
-        except KeyError:
-            pass
-
         self.all_scholars.append(cur_scholar_json)
 
+    def get_name(self, scholar, query_id):
+        try:
+            return scholar.attributes["aliases"]["en"][0]["value"]
+        except (TypeError, KeyError):
+            try:
+                return scholar.attributes["labels"]["en"]["value"]
+            except (TypeError, KeyError):
+                self.debug.write("NO NAME: " + query_id + "\n")
+                return ""
+
+    def get_wiki_link(self, scholar, query_id):
+        try:
+            return scholar.attributes["sitelinks"]["enwiki"]["url"]
+        except (TypeError, KeyError):
+            self.debug.write("NO WIKI LINK: " + query_id + "\n")
+            return ""
+
+    def get_image(self, scholar, query_id):
+        try:
+            return WIKIDATA_IMAGE + scholar.attributes["claims"][property_map["image"]
+                                                                 ][0]["mainsnak"]["datavalue"]["value"].replace(" ", "_")
+        except (TypeError, KeyError):
+            self.debug.write("NO IMAGE: " + query_id + "\n")
+            return ""
+
+    def get_field(self, scholar, query_id):
+        try:
+            return scholar.attributes["claims"][property_map["field"]][0
+                                                                       ]["mainsnak"]["datavalue"]["value"]["id"]
+        except (TypeError, KeyError):
+            self.debug.write("NO FIELD: " + query_id + "\n")
+            return ""
+    
+    def get_advisors(self, scholar, query_id):
+        advisors = list()
+        try:
+            for advisor in scholar.attributes["claims"][property_map["doctoral_advisor"]]:
+                try:
+                    advisor_qid = advisor["mainsnak"]["datavalue"]["value"]["id"]
+                    self.to_process.append(advisor_qid)
+                    advisors.append(advisor_qid)
+                except KeyError:
+                    pass
+        except KeyError:
+            pass
+        return advisors
+    
+    def get_students(self, scholar, query_id):
+        students = list()
+        try:
+            for student in scholar.attributes["claims"][property_map["doctoral_student"]]:
+                try:
+                    student_qid = student["mainsnak"]["datavalue"]["value"]["id"]
+                    self.to_process.append(student_qid)
+                    students.append(student_qid)
+                except KeyError:
+                    pass
+        except KeyError:
+            pass
+        return students
+
     def finish_and_print(self):
-        self.scholar_json.write(json.dumps(self.all_scholars, indent=4, sort_keys=True))
+        self.scholar_json.write(json.dumps(
+            self.all_scholars, indent=4, sort_keys=True))
         self.debug.close()
         self.scholar_json.close()
 
