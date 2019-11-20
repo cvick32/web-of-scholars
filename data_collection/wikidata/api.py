100efa3d05e74a36c1776cbcb7c9bf8de7ed2cb7
cole <cole.d.vick@gmail.com>
2019-11-20 10:38:40 -0500

    abstract try-catch from individual methods

--- data_collection/wikidata/api.py
+++ data_collection/wikidata/api.py
@@ -10,42 +10,60 @@ property_map = {
 
 WIKIDATA_IMAGE = "https://commons.wikimedia.org/wiki/File:"
 
-
 class WikiDataScholars:
-    def __init__(self):
-        # open log files
-        self.debug = open("debug.txt", "w+")
+    def __init__(self, starting_scholars=["Q222541"], scholar_json_file="scholars.json", debug_file="debug.txt", max_scholars=0, bounded_huh=True):
+        """
+        Creates a new instance of a WikiDataScholar object. 
+
+        starting_scholars: initial list of scholars to begin the web from
+        scholar_json_file: file path for the JSON object of the web
+        debug_file: file path for debug file
+        max_scholars: how many scholars we want to scrape before exiting
+        bounded_huh: do we want to stop on a bound?
+        """
+        self.debug = open(debug_file, "w+")
         self.debug.seek(0)
         self.debug.truncate()
 
-        self.scholar_json = open("scholars.json", "w+")
+        self.scholar_json = open(scholar_json_file, "w+")
         self.scholar_json.seek(0)
         self.scholar_json.truncate()
 
         self.Wiki_Client = Client()
 
-        self.to_process = ["Q222541"]
+        self.to_process = starting_scholars
         self.seen_scholars = set()
         self.all_scholars = list()
 
+        self.max_scholars = max_scholars
+        self.bounded_huh = bounded_huh
+
+        self.num_processed = 0
+
     def run(self):
+        """Begin the scraping process for the web."""
         while self.to_process:
+            if self.bounded_huh and self.num_processed >= self.max_scholars:
+                break
             if not self.to_process[0] in self.seen_scholars:
                 self.seen_scholars.add(self.to_process[0])
                 self.get_scholar_advisors_and_students(self.to_process[0])
             self.to_process = self.to_process[1:]
+            self.num_processed += 1
         self.finish_and_print()
 
     def get_scholar_advisors_and_students(self, scholar_query_id):
         cur_scholar = self.Wiki_Client.get(scholar_query_id)
-        print(scholar_query_id)
 
-        scholar_name = self.get_name(cur_scholar, scholar_query_id)
+        scholar_name      = self.get_name(cur_scholar, scholar_query_id)
         scholar_wiki_link = self.get_wiki_link(cur_scholar, scholar_query_id)
-        scholar_image = self.get_image(cur_scholar, scholar_query_id)
-        scholar_field = self.get_field(cur_scholar, scholar_query_id)
-        scholar_advisors = self.get_advisors(cur_scholar, scholar_query_id)
-        scholar_students = self.get_students(cur_scholar, scholar_query_id)
+        scholar_image     = self.get_image(cur_scholar, scholar_query_id)
+        scholar_field     = self.get_field(cur_scholar, scholar_query_id)
+        scholar_advisors  = self.get_advisors(cur_scholar, scholar_query_id)
+        scholar_students  = self.get_students(cur_scholar, scholar_query_id)
+
+        self.debug.write(f"Scholar Name: {scholar_name}\n")
+        self.debug.write(f"Scholar Query ID: {scholar_query_id}\n")
 
         cur_scholar_json = {
             "id": scholar_query_id,
@@ -58,65 +76,52 @@ class WikiDataScholars:
         }
         self.all_scholars.append(cur_scholar_json)
 
-    def get_name(self, scholar, query_id):
+    
+    def get_any_from_attributes(self, attributes, items, desired, query_id):
         try:
-            return scholar.attributes["aliases"]["en"][0]["value"]
+            current_attr = attributes
+            for item in items:
+                current_attr = current_attr.__getitem__(item)
+            return current_attr
         except (TypeError, KeyError):
-            try:
-                return scholar.attributes["labels"]["en"]["value"]
-            except (TypeError, KeyError):
-                self.debug.write("NO NAME: " + query_id + "\n")
-                return ""
-
-    def get_wiki_link(self, scholar, query_id):
-        try:
-            return scholar.attributes["sitelinks"]["enwiki"]["url"]
-        except (TypeError, KeyError):
-            self.debug.write("NO WIKI LINK: " + query_id + "\n")
-            return ""
-
-    def get_image(self, scholar, query_id):
-        try:
-            return WIKIDATA_IMAGE + scholar.attributes["claims"][property_map["image"]
-                                                                 ][0]["mainsnak"]["datavalue"]["value"].replace(" ", "_")
-        except (TypeError, KeyError):
-            self.debug.write("NO IMAGE: " + query_id + "\n")
-            return ""
-
-    def get_field(self, scholar, query_id):
-        try:
-            return scholar.attributes["claims"][property_map["field"]][0
-                                                                       ]["mainsnak"]["datavalue"]["value"]["id"]
-        except (TypeError, KeyError):
-            self.debug.write("NO FIELD: " + query_id + "\n")
+            self.debug.write(f"NO {desired}: {query_id}\n")
             return ""
     
+    def get_name(self, scholar, query_id):
+        name = self.get_any_from_attributes(scholar.attributes, ["aliases", "en", 0, "value"], "name", query_id)
+        if not name:
+            return self.get_any_from_attributes(scholar.attributes, ["labels", "en", "value"], "name", query_id)
+        else:
+            return name
+
+    def get_wiki_link(self, scholar, query_id):
+        return self.get_any_from_attributes(scholar.attributes, ["sitelinks", "enwiki", "url"], "wiki_link", query_id)
+
+    def get_image(self, scholar, query_id):
+        image_link = self.get_any_from_attributes(scholar.attributes, ["claims", property_map["image"], 0, "mainsnak", "datavalue", "value"], "image", query_id)
+        return WIKIDATA_IMAGE + image_link.replace(" ", "_")
+    
+    def get_field(self, scholar, query_id):
+        return self.get_any_from_attributes(scholar.attributes, ["claims", property_map["field"], 0, "mainsnak", "datavalue", "value", "id"], "field", query_id)
+       
     def get_advisors(self, scholar, query_id):
         advisors = list()
-        try:
-            for advisor in scholar.attributes["claims"][property_map["doctoral_advisor"]]:
-                try:
-                    advisor_qid = advisor["mainsnak"]["datavalue"]["value"]["id"]
-                    self.to_process.append(advisor_qid)
-                    advisors.append(advisor_qid)
-                except KeyError:
-                    pass
-        except KeyError:
-            pass
+        for advisor in self.get_any_from_attributes(scholar.attributes, ["claims", property_map["doctoral_advisor"]], "advisors", query_id):
+            advisor_qid = self.get_any_from_attributes(advisor, ["mainsnak", "datavalue", "value", "id"], "advisor query ids", query_id)
+            if advisor_qid:
+                self.to_process.append(advisor_qid)
+                advisors.append(advisor_qid)
         return advisors
     
     def get_students(self, scholar, query_id):
         students = list()
-        try:
-            for student in scholar.attributes["claims"][property_map["doctoral_student"]]:
-                try:
-                    student_qid = student["mainsnak"]["datavalue"]["value"]["id"]
-                    self.to_process.append(student_qid)
-                    students.append(student_qid)
-                except KeyError:
-                    pass
-        except KeyError:
-            pass
+        
+        for student in self.get_any_from_attributes(scholar.attributes, ["claims", property_map["doctoral_student"]], "students", query_id):
+            student_qid = self.get_any_from_attributes(student, ["mainsnak", "datavalue", "value", "id"], "student query id", query_id)
+            if student_qid:
+                self.to_process.append(student_qid)
+                students.append(student_qid)
+               
         return students
 
     def finish_and_print(self):
@@ -126,5 +131,5 @@ class WikiDataScholars:
         self.scholar_json.close()
 
 
-scholar_web = WikiDataScholars()
+scholar_web = WikiDataScholars(max_scholars=5)
 scholar_web.run()
