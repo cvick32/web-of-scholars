40e3691583b26efef7120a359a9b1af709ee365f
cole <cole.d.vick@gmail.com>
2020-11-05 22:01:51 -0600

    generate 200 scholars

--- generate_scholars.py
+++ generate_scholars.py
@@ -6,5 +6,5 @@ change max_scholars to have more or fewer scholars
 """
 
 wd = WikiDataScholars(scholar_json_file="data_collection/scholars.json")
-wd.run(max_scholars=50)
-wd.finish_and_print()
+scholars = wd.run(max_scholars=200)
+wd.finish_and_print(scholars)
